9175da4b7765dad2f7610704b1e0764eed1b603b
alex-kurkov <alexkourkov@yandex.ru>
2023-08-22 15:30:05 +0500

    chore: add deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: '/src',
+      assets: '/src/assets',
+      components: '/src/components',
+      context: '/src/context',
+      pages: '/src/pages',
+      utils: '/src/utils',
+    },
+  },
+});
